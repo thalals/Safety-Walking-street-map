1293eaf04d313863fe6b450618da8c7c337bd066
thalals <thalsal@naver.com>
2021-02-05 17:10:55 +0900

    basic

---
+++ start_study/myproject/firstapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Designer(models.Model) :  #models.Model은 고정(형식)
+    image = models.ImageField(null=True, blank=True, upload_to ="images/")  #미디어에 있는 images에 올릴거다
+    name = models.CharField(max_length=50)
+    adress = models.CharField(max_length=50)
+    description = models.TextField()
+
+    #생성되는 객체의 이름 object -> name
+    def __str__(self) :
+        return self.name
