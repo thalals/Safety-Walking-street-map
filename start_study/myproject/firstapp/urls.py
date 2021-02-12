7e6dfc1e4b4e383fc43932729d12e33c7cc55cc4
thalals <thalsal@naver.com>
2021-02-12 23:03:01 +0900

    Genefic view CBV(List)

--- start_study/myproject/firstapp/urls.py
+++ start_study/myproject/firstapp/urls.py
@@ -4,5 +4,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.home, name ='home'),
+    # path('',views.home, name ='home'),
+    path('',views.DesignerList.as_view(), name='designer'),
+    path('home',views.go, name='go'),
+    path('create',views.DesignerCreate.as_view, name='create'),
 ]
--- start_study/myproject/firstapp/views.py
+++ start_study/myproject/firstapp/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
 from . import models
+#CBV 사용
+from django.views.generic import CreateView,ListView
 
-# Create your views here.
-def home(request):
-    designer = models.Designer.objects.all()
-    print(designer)
-    return render(request,'home.html', {'designer' : designer})
+# # fbv.
+# def home(request):
+#     designer = models.Designer.objects.all()
+#     print(designer)
+#     return render(request,'home.html', {'designer' : designer})
+
+def go(request):
+    return render(request,'create.html')
+#cbv
+#Read(게시물 나열)
+class DesignerList(ListView):
+    model = models.Designer
+    context_object_name = 'designer'   #객체를 부르는 이름
+    template_name='home.html'    #Default 연결 값 변경
+
+
+#Create(게시물 생성)
+class DesignerCreate(CreateView):
+    model = models.Designer
+    context_object_name = 'designer'
+    success_url='home.html'
+    template_name='create.html'
