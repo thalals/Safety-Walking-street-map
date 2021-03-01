513b3a1ba01a8daeccc9ddec8fbfb81c3432705e
thalals <thalsal@naver.com>
2021-03-01 22:27:17 +0900

    [ADD] Kakao map platform

--- map_api_study/KakaoMap/kakaoapp/views.py
+++ map_api_study/KakaoMap/kakaoapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(reqeust):
     return render (reqeust, "index.html")
+
+def test(request):
+    return render (request,"test.html")
