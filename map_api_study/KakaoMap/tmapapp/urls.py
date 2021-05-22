1a8b898f879a559c965101266d59434bedfb23a6
thalals <thalsal@naver.com>
2021-05-23 01:30:10 +0900

    astar url 연결

--- map_api_study/KakaoMap/tmapapp/urls.py
+++ map_api_study/KakaoMap/tmapapp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('pathfinder/',views.pathSetting,name='pathSetting'),
     path('pathFinder/',views.pathFinder,name='pathFinder'),#길찾기 구 반환
     path('normalPath/',views.normalPath,name='normalPath'),#일반 길찾기
-    path('hardPath/',views.aStar,name='aStar'),#우회 길찾기
+    path('hardPath/',views.origin_Astar,name='aStar'),#우회 길찾기
     path('tsample',views.tsample, name='tsample'),
 
     path('grid_draw/',views.gird_draw, name='grid_draw') #그리드 그리기
